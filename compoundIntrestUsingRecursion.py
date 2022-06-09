543cb8158e62ab8bd3f045b0697bb4a3f07b5d3b
NANDIMANDALAM-SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-06-09 11:42:17 +0530

    Update compoundIntrestUsingRecursion.py

--- compoundIntrestUsingRecursion.py
+++ compoundIntrestUsingRecursion.py
@@ -1,9 +1,10 @@
-def compoundIntrest(principal, rate, time):
-    if time == 1:
+
+principal=int(input("Enter amount to calculate the compound intrest:"))
+rate=int(input("Enter the rate : "))
+year=float(input("Enter the years : "))
+def compoundIntrest(principal,rate,year):
+    if year<=0:
         return principal
     else:
-        return principal + compoundIntrest(principal, rate, time - 1) * (1 + rate)
-principal = int(input("Enter the principal amount: "))
-rate = float(input("Enter the rate of interest: "))
-time = int(input("Enter the time in years: "))
-print("The compound interest is", compoundIntrest(principal, rate, time))
+        return compoundIntrest(principal+principal*rate/100,rate,year-1)
+print(compoundIntrest(principal,rate,year))
